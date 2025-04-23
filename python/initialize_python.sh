417f7c5165c578103f4c5743c40e1f8b9ede4abe
Andy <andynu@gmail.com>
2025-04-23 11:54:12 -0400

    Only create the venv if it isn't already there.

--- python/initialize_python.sh
+++ python/initialize_python.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-set ex
-python -mvenv venv
+set -e
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
 source venv/bin/activate
+
+# Upgrade pip to latest version
+python -m pip install --upgrade pip
+
+# Install requirements
 pip install -r requirements.txt
